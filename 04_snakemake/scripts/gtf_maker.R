6312ea11d2476f34b732f916b9b35a4aee71a433
Izaskun Mallona <izaskun.mallona@gmail.com>
2021-09-13 17:05:43 +0200

    Fix the gtf maker regexp, so it handles X/Y chromosomes properly

--- 04_snakemake/scripts/gtf_maker.R
+++ 04_snakemake/scripts/gtf_maker.R
@@ -20,6 +20,9 @@ option_list = list(
 opt_parser <- OptionParser(option_list=option_list)
 opt <- parse_args(opt_parser)
 
+## debugging
+if (FALSE)
+    opt <- list(fai = '/home/imallona/repeats_sc/annotation/repeatome_from_hg38_rmsk.gtf.fa.fai')
 ## print(sprintf('processing GTF %s', opt$gtf))
 
 con <- file(opt$fai, open = "r")
@@ -43,9 +46,12 @@ while (TRUE) {
     ## USE THIS FOR LOCUS-BASED REPEATOMES
     ## parsed <- c(line[1], 'repeat', 'exon', "1", line[2], "0.000000", '+', '.', sprintf('gene_id "%s"; transcript_id "%s";', locus, line[1]))
 
-    family <- gsub("(^.*)_([chr]?\\d*)_(\\d*)_(\\d*)_([-+]{1}$)", "\\1", line[1], perl = TRUE)
+    family <- gsub("(^.*)_([chr]?[\\d|X|x|Y|y]*)_(\\d*)_(\\d*)_([-+]{1}$)", "\\1", line[1], perl = TRUE)
     
     ## USE THIS FOR NAME-BASED REPEATOMES
+    if (grepl('\\+$|\\-$', family)) {
+        stop('something wrong here!')
+    }
     parsed <- c(line[1], 'repeat', 'exon', "1", line[2], "0.000000", '+', '.', sprintf('gene_id "%s"; transcript_id "%s";', family, line[1]))
     cat(sprintf('%s\n', paste(parsed, collapse = '\t')))
 }
